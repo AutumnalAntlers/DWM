9d1eb4f1b58bbdb76bcb3eecea8dbba786546f1a
AutumnalAntlers <AutumnalAntlers@gmail.com>
2020-04-01 16:26:32 -0700

    Removing unused layouts

--- vanitygaps.c
+++ vanitygaps.c
@@ -9,15 +9,11 @@ static void incrihgaps(const Arg *arg);
 static void incrivgaps(const Arg *arg);
 static void togglegaps(const Arg *arg);
 /* Layouts */
-static void bstack(Monitor *m);
-static void bstackhoriz(Monitor *m);
 static void centeredmaster(Monitor *m);
 static void centeredfloatingmaster(Monitor *m);
 static void deck(Monitor *m);
 static void dwindle(Monitor *m);
 static void fibonacci(Monitor *m, int s);
-static void grid(Monitor *m);
-static void nrowgrid(Monitor *m);
 static void spiral(Monitor *m);
 static void tile(Monitor *m);
 /* Internals */
