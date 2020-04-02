8d99cdc0829854a0dc0fc7562680df54c5c2178c
AutumnalAntlers <AutumnalAntlers@gmail.com>
2020-04-01 18:09:46 -0700

    Removed GIMP rule

--- config.def.h
+++ config.def.h
@@ -37,7 +37,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,      "Library",   0,            1,           -1, },
 	{ "Wpg",      "wpg",      NULL,       0,            1,           -1, },
