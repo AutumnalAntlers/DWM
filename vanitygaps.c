2288fc4c0c975e948cef8e5abcbb52a2f8a7eb9f
AutumnalAntlers <AutumnalAntlers@gmail.com>
2020-04-29 14:31:39 -0700

    Mid-screen border is done

--- vanitygaps.c
+++ vanitygaps.c
@@ -435,12 +435,12 @@ tile(Monitor *m)
 	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
 			if(n == 1)
-				resize(c, mx, my, mw - (2*c->bw), mh * (c->cfact / mfacts) - (2*c->bw), 0);
+				resize(c, mx - c->bw, my, mw - (2*c->bw), mh * (c->cfact / mfacts) - (2*c->bw), 0);
 			else
-				resize(c, mx, my, mw - (2*c->bw), mh * (c->cfact / mfacts) - (2*c->bw), 0);
+				resize(c, mx - c->bw, my, mw - (2*c->bw), mh * (c->cfact / mfacts) - (2*c->bw), 0);
 			my += HEIGHT(c) + ih - c->bw;
 		} else {
-			resize(c, sx, sy, sw - (2*c->bw), sh * (c->cfact / sfacts) - (2*c->bw), 0);
+			resize(c, sx - c->bw, sy, sw - (2*c->bw), sh * (c->cfact / sfacts) - (2*c->bw), 0);
 			sy += HEIGHT(c) + ih - c->bw;
 		}
 }
